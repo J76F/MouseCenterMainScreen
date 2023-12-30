18dc4696e308fb8b28575c2d2e55229375ba37d1
Jeroen Faber <105970388+J76F@users.noreply.github.com>
2023-12-30 03:05:42 +0100

    Update

---
+++ mousecentermonitor2.ps1
@@ -0,0 +1,5 @@
+$bounds = [system.windows.forms.screen]::AllScreens[1].Bounds
+$center = $bounds.Location
+$center.X += $bounds.Width / 2
+$center.Y += $bounds.Height / 2
+[System.Windows.Forms.Cursor]::Position = $center
